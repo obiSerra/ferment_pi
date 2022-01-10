2a640edc362122f1250a183c00e756a5ed65e25b
Roberto Serra <roberto.serra@etiqa.it>
2022-01-10 20:45:21 +0100

    temperature read

--- screen.py
+++ screen.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-import signal
-from  sh1106 import SH1106
 import time
-from sh1106 import config
+import signal
+from sh1106 import SH1106
 
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
-FONT_DIR='assets'
+FONT_DIR = 'assets'
+
 
 class Screen():
 
@@ -22,40 +22,44 @@ class Screen():
     @property
     def width(self):
         return self.disp.width
-    
+
     @property
     def height(self):
         return self.disp.height
-        
+
     def new_screen(self, bg_color=1):
-        self.image = Image.new('1', (self.disp.width, self.disp.height), bg_color)
+        self.image = Image.new(
+            '1', (self.disp.width, self.disp.height), bg_color)
 
     def render(self):
         self.disp.ShowImage(self.disp.getbuffer(self.image))
         time.sleep(1)
 
-    def draw_border(self, top=True, bottom=True, left=True, right=True, fill=0):
+    def draw_border(self, top=True, bottom=True, left=True, right=True,
+                    fill=0):
         draw = ImageDraw.Draw(self.image)
         if top:
-            draw.line([(0,0),(127,0)], fill = fill)
+            draw.line([(0, 0), (127, 0)], fill=fill)
         if left:
-            draw.line([(0,0),(0,63)], fill = fill)
+            draw.line([(0, 0), (0, 63)], fill=fill)
         if bottom:
-            draw.line([(0,63),(127,63)], fill = fill)
+            draw.line([(0, 63), (127, 63)], fill=fill)
         if right:
-            draw.line([(127,0),(127,63)], fill = fill)
+            draw.line([(127, 0), (127, 63)], fill=fill)
 
-    def draw_text(self, text, font_size=13, position=(0, 0), font='Font.ttf', bg_color=1, fill=0):
+    def draw_text(self, text, font_size=13, position=(0, 0), font='Font.ttf',
+                  bg_color=1, fill=0):
         if not self.image:
             self.new_screen(bg_color)
-        draw = ImageDraw.Draw(self.image)    
+        draw = ImageDraw.Draw(self.image)
         font = ImageFont.truetype('{}/{}'.format(FONT_DIR, font), font_size)
-        draw.text(position, text, font = font, fill = fill)
+        draw.text(position, text, font=font, fill=fill)
 
 
 def quit_gracefully(*args):
-    #epdconfig.module_exit()
+    # epdconfig.module_exit()
     exit()
+
 
 signal.signal(signal.SIGINT, quit_gracefully)
 
@@ -68,7 +72,7 @@ if __name__ == '__main__':
 
     except IOError as e:
         print(e)
-        
-    except KeyboardInterrupt:    
+
+    except KeyboardInterrupt:
         print("ctrl + c:")
         quit_gracefully()
--- temperature.py
+++ temperature.py
@@ -6,41 +6,47 @@ import board
 import adafruit_dht
 
 
+class Temperature:
+    def __init__(self, pin):
+        self.dht_device = adafruit_dht.DHT11(pin)
+
+    def read(self):
+        temperature_c = self.dht_device.temperature
+        humidity = self.dht_device.humidity
+        return {'temperature': temperature_c, 'humidity': humidity}
+
+    def exit(self):
+        self.dht_device.exit()
+
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4)
+
 
 # you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
 # This may be necessary on a Linux single board computer like the Raspberry Pi,
 # but it will not work in CircuitPython.
 # dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-def get_temperature_data():
-    temperature_c = dhtDevice.temperature
-    humidity = dhtDevice.humidity
-    return "Temperature {:.1f} C Humidity {}%".format(temperature_c, humidity)
 
+if __name__ == '__main__':
+    tem = Temperature(board.D4)
 
-#if __name__ == '__main__':
+    while True:
+        try:
 
-while True:
-    try:
-        # Print the values to the serial port
-
-        temperature_f = temperature_c * (9 / 5) + 32
-
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+            temp_data = tem.read()
+            print(
+                "Temp: {:.1f} C    Humidity: {}% ".format(
+                    temp_data.temperature, temp_data.humidity
+                )
             )
-        )
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
+        except RuntimeError as error:
+            # Errors happen fairly often, DHT's are hard to read, just keep going
+            print(error.args[0])
+            time.sleep(2.0)
+            continue
+        except Exception as error:
+            tem.exit()
+            raise error
+
         time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-
-    time.sleep(2.0)
