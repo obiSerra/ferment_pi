c58c92288c9032c6669f5663d8cdeceb1b0924c8
Roberto Serra <obi.serra@gmail.com>
2022-01-10 18:41:24 +0000

    first commit

---
+++ temperature.py
@@ -0,0 +1,46 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import adafruit_dht
+
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+def get_temperature_data():
+    temperature_c = dhtDevice.temperature
+    humidity = dhtDevice.humidity
+    return "Temperature {:.1f} C Humidity {}%".format(temperature_c, humidity)
+
+
+#if __name__ == '__main__':
+
+while True:
+    try:
+        # Print the values to the serial port
+
+        temperature_f = temperature_c * (9 / 5) + 32
+
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(2.0)
