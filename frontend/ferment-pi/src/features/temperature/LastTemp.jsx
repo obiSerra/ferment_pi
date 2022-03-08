569a50566a55f4d3dd9e68d86448bb3a1761dad5
Roberto Serra <roberto.serra@etiqa.it>
2022-03-08 14:45:40 +0100

    mock store added

--- frontend/ferment-pi/src/features/temperature/LastTemp.jsx
+++ frontend/ferment-pi/src/features/temperature/LastTemp.jsx
@@ -1,15 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { lastTempAction, selectLastTemp } from "./temperatureSlice";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectLastTemp } from "./temperatureSlice";
 
 export default function LastTemp() {
   const lastTemp = useSelector(selectLastTemp);
 
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(lastTempAction());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
   return <div>Last temperature: {lastTemp?.temperature} C</div>;
 }
